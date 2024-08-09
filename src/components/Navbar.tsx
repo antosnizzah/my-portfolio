88523baf0fb4ca6b031c51bcbc6b8bd25c4de74c
antosnizzah <antonygatitu327@gmail.com>
2024-08-09 11:30:03 +0300

    done some updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,8 +24,8 @@ const Navbar: React.FC = () => {
 
   return (
     <div className="flex justify-center items-center">
-      <nav className="navbar bg-base-100 rounded-lg shadow-lg lg:flex hidden">
-        <div className="relative flex justify-center items-center w-full max-w-[420px] h-[70px] bg-[#209cff] rounded-full mx-auto">
+      <nav className="navbar bg-transparent rounded-lg shadow-lg flex">
+        <div className="relative flex justify-center items-center w-full max-w-[420px] h-[70px] bg-[#1E293B] rounded-full mx-auto p-1">
           <ul className="flex w-full justify-around">
             {navItems.map((item, index) => (
               <li
@@ -47,21 +47,18 @@ const Navbar: React.FC = () => {
               transform: `translateX(${activeIndex * 70}px)`,
             }}
           >
-            <div className="absolute top-1/2 left-1/2 w-[50px] h-[50px] bg-[#209cff] rounded-full transform -translate-x-1/2 -translate-y-1/2"></div>
+            <div className="absolute top-1/2 left-1/2 w-[50px] h-[50px] bg-[#1E293B] rounded-full transform -translate-x-1/2 -translate-y-1/2"></div>
           </div>
         </div>
         <div className="flex items-center space-x-4 ml-5">
-          <button onClick={toggleDarkMode} className="p-2 rounded-full bg-gray-200 dark:bg-gray-700 transition-colors duration-300">
+          <button
+            onClick={toggleDarkMode}
+            className="p-2 rounded-full bg-gray-200 dark:bg-gray-700 transition-colors duration-300"
+          >
             {darkMode ? <Sun className="text-yellow-500" /> : <Moon className="text-gray-900" />}
           </button>
         </div>
       </nav>
-      <div className="lg:hidden">
-        {/* Button to toggle the navbar on small screens */}
-        <button className="btn btn-square btn-primary" onClick={toggleDarkMode}>
-          {darkMode ? <Sun className="w-6 h-6" /> : <Moon className="w-6 h-6" />}
-        </button>
-      </div>
     </div>
   );
 };
